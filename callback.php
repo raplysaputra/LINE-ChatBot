5771dd38d9e4c6dc2338357fc66f97d6faf89c97
raplysaputra <plankuye@gmail.com>
2017-01-15 11:10:56 +0700

    Create callback.php

---
+++ callback.php
@@ -0,0 +1,31 @@
+/* 輸入申請的Line Developers 資料  */
+$channel_id = "1496480448";
+$channel_secret = "0c7123015d1d6a85826e21a408628b7d";
+$mid = "MID";
+ 
+/* 將收到的資料整理至變數 */
+$receive = json_decode(file_get_contents("php://input"));
+$text = $receive->result{0}->content->text;
+$from = $receive->result[0]->content->from;
+$content_type = $receive->result[0]->content->contentType;
+ 
+/* 準備Post回Line伺服器的資料 */
+$header = ["Content-Type: application/json; charser=UTF-8", "X-Line-ChannelID:" . $channel_id, "X-Line-ChannelSecret:" . $channel_secret, "X-Line-Trusted-User-With-ACL:" . $mid];
+$message = getBoubouMessage($text);
+sendMessage($header, $from, $message);
+ 
+ 
+/* 發送訊息 */
+function sendMessage($header, $to, $message) {
+ 
+	$url = "https://trialbot-api.line.me/v1/events";
+	$data = ["to" => [$to], "toChannel" => 1383378250, "eventType" => "138311608800106203", "content" => ["contentType" => 1, "toType" => 1, "text" => $message]];
+	$context = stream_context_create(array(
+	"http" => array("method" => "POST", "header" => implode(PHP_EOL, $header), "content" => json_encode($data), "ignore_errors" => true)
+	));
+	file_get_contents($url, false, $context);
+}
+ 
+function getBoubouMessage($value){		
+	return "寶寶" . $value ."，只是寶寶不說";
+}
